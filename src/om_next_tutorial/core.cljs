c51585ea63ae6b0995a9bb15a1b60c144e7c679d
griffio <griffio@users.noreply.github.com>
2015-10-15 13:30:59 +0100

    step 2

--- src/om_next_tutorial/core.cljs
+++ src/om_next_tutorial/core.cljs
@@ -1,15 +1,17 @@
 (ns om-next-tutorial.core
-(:require [goog.dom :as gdom]
-          [om.next :as om :refer-macros [defui]]
-          [om.dom :as dom]))
-
-(enable-console-print!)
+  (:require [goog.dom :as gdom]
+            [om.next :as om :refer-macros [defui]]
+            [om.dom :as dom]))
 
 (defui HelloWorld
-  Object
-  (render [this]
-          (dom/div nil "Hello world!")))
+       Object
+       (render [this]
+               (dom/div nil (get (om/props this) :title))))
 
 (def hello (om/factory HelloWorld))
 
-(js/React.render (hello) (gdom/getElement "app"))
+(js/React.render
+  (apply dom/div nil
+         (map #(hello {:title (str "Hello " %)})
+              (range 6)))
+  (gdom/getElement "app"))
