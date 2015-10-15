5bb2eb0fc7340f69f65d6eec4dec3265a4a4238e
griffio <griffio@users.noreply.github.com>
2015-10-15 13:34:34 +0100

    step 4 - reconciler (read, mutate)
    om/parser

--- src/om_next_tutorial/core.cljs
+++ src/om_next_tutorial/core.cljs
@@ -5,7 +5,22 @@
 
 (def app-state (atom {:count 0}))
 
+(defn read [{:keys [state] :as env} key params]
+  (let [st @state]
+    (if-let [[_ value] (find st key)]
+      {:value value}
+      {:value :not-found})))
+
+(defn mutate [{:keys [state] :as env} key params]
+  (if (= 'increment key)
+    {:value  [:count]
+     :action #(swap! state update-in [:count] inc)}
+    {:value :not-found}))
+
 (defui Counter
+       static om/IQuery
+       (query [this]
+              [:count])
        Object
        (render [this]
                (let [{:keys [count]} (om/props this)]
@@ -13,12 +28,13 @@
                           (dom/span nil (str "Count: " count))
                           (dom/button
                             #js {:onClick
-                                 (fn [e]
-                                   (swap! app-state update-in [:count] inc))}
+                                 (fn [_] (om/transact! this '[(increment)]))}
                             "Click me!")))))
 
 (def reconciler
-  (om/reconciler {:state app-state}))
+  (om/reconciler
+    {:state  app-state
+     :parser (om/parser {:read read :mutate mutate})}))
 
 (om/add-root! reconciler
               Counter (gdom/getElement "app-counter"))
