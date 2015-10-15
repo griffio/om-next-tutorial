7d63de7f719d24178970086a4ddc500f5f0655ab
griffio <griffio@users.noreply.github.com>
2015-10-15 13:33:31 +0100

    step 3 - reconciler

--- src/om_next_tutorial/core.cljs
+++ src/om_next_tutorial/core.cljs
@@ -3,6 +3,26 @@
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
+(def app-state (atom {:count 0}))
+
+(defui Counter
+       Object
+       (render [this]
+               (let [{:keys [count]} (om/props this)]
+                 (dom/div nil
+                          (dom/span nil (str "Count: " count))
+                          (dom/button
+                            #js {:onClick
+                                 (fn [e]
+                                   (swap! app-state update-in [:count] inc))}
+                            "Click me!")))))
+
+(def reconciler
+  (om/reconciler {:state app-state}))
+
+(om/add-root! reconciler
+              Counter (gdom/getElement "app-counter"))
+
 (defui HelloWorld
        Object
        (render [this]
