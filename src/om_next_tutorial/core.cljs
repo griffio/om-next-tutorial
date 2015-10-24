e9052bee8f0ec585b06bf25974ac9b28c405571b
griffio <griffio@users.noreply.github.com>
2015-10-24 19:15:26 +0100

    dashboard render
    https://github.com/omcljs/om/wiki/Queries-With-Unions

--- src/om_next_tutorial/core.cljs
+++ src/om_next_tutorial/core.cljs
@@ -259,17 +259,43 @@
 (defui Post
        static om/IQuery
        (query [this]
-              [:id :type :title :author :content]))
+              [:id :type :title :author :content])
+       Object
+       (render [this]
+               (let [{:keys [title author content] :as props} (om/props this)]
+                 (dom/div nil
+                          (dom/h3 nil title)
+                          (dom/h4 nil author)
+                          (dom/p nil content)))))
+
+(def post (om/factory Post))
 
 (defui Photo
        static om/IQuery
        (query [this]
-              [:id :type :title :image :caption]))
+              [:id :type :title :image :caption])
+       Object
+       (render [this]
+               (let [{:keys [title image caption]} (om/props this)]
+                 (dom/div nil
+                          (dom/h3 nil (str "Photo: " title))
+                          (dom/div nil image)
+                          (dom/p nil "Caption: ")))))
+
+(def photo (om/factory Photo))
 
 (defui Graphic
        static om/IQuery
        (query [this]
-              [:id :type :image]))
+              [:id :type :image])
+       Object
+       (render [this]
+               (let [{:keys [title image]} (om/props this)]
+                 (dom/div nil
+                          (dom/h3 nil (str "Graphic: " title))
+                          (dom/div nil image)))))
+
+(def graphic (om/factory Graphic))
 
 (defui DashboardItem
        static om/Ident
@@ -282,12 +308,38 @@
                 (map #(conj % :favorites)
                      [(om/get-query Post)
                       (om/get-query Photo)
-                      (om/get-query Graphic)]))))
+                      (om/get-query Graphic)])))
+       Object
+       (render [this]
+               (let [{:keys [id type favorites] :as props} (om/props this)]
+                 (dom/li
+                   #js {:style #js {:padding 10 :borderBottom "1px solid black"}}
+                   (dom/div nil
+                            (({:dashboard/post    post
+                               :dashboard/photo   photo
+                               :dashboard/graphic graphic} type)
+                              (om/props this)))
+                   (dom/div nil
+                            (dom/p nil (str "Favourites: " favorites))
+                            (dom/button
+                              #js {:onClick
+                                   (fn [e]
+                                     (om/transact! this
+                                                   `[(dashboard/favorite {:ref [~type ~id]})]))}
+                              "Favorite!"))))))
+
+(def dashboard-item (om/factory DashboardItem))
 
 (defui Dashboard
        static om/IQuery
        (query [this]
-              [{:dashboard/items (om/get-query DashboardItem)}]))
+              [{:dashboard/items (om/get-query DashboardItem)}])
+       Object
+       (render [this]
+               (let [{:keys [dashboard/items]} (om/props this)]
+                 (apply dom/ul
+                        #js {:style #js {:padding 0}}
+                        (map dashboard-item items)))))
 
 (defmulti read-dashboard om/dispatch)
 
@@ -303,4 +355,20 @@
                          #(->> (for [[k v] %]
                                  [k (into [] (remove #{:favorites}) v)])
                                (into {})))}))
+
+(defmulti mutate-dashboard om/dispatch)
+
+(defmethod mutate-dashboard 'dashboard/favorite
+  [{:keys [state]} k {:keys [ref]}]
+  {:action
+   (fn []
+     (swap! state update-in (conj ref :favorites) inc))})
+
+(def reconciler
+  (om/reconciler
+    {:state init-dashboard-data
+     :parser (om/parser {:read read-dashboard :mutate mutate-dashboard})}))
+
+(om/add-root! reconciler Dashboard (gdom/getElement "dashboard"))
+
 ;;=======================================================
