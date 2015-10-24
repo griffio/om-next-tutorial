6dcae838066b22549338a7a421a2b955b34bf81a
griffio <griffio@users.noreply.github.com>
2015-10-24 09:30:50 +0100

    1.0.0-alpha7
    1.0.0-alpha7 updated

--- project.clj
+++ project.clj
@@ -3,6 +3,5 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
                  [datascript "0.13.1"]
-                 [org.omcljs/om "1.0.0-alpha5"]
+                 [org.omcljs/om "1.0.0-alpha7"]
                  [figwheel-sidecar "0.4.0" :scope "provided"]])
-
--- src/om_next_tutorial/core.cljs
+++ src/om_next_tutorial/core.cljs
@@ -73,13 +73,13 @@
 ;; People
 ;;===============================
 (def init-data
-  {:list/one [{:name "John" :points 0}
+  {:list/one [{:name "Yoda" :points 0 :age 800}
               {:name "Mary" :points 0}
               {:name "Bob" :points 0}]
    :list/two [{:name "Mary" :points 0 :age 27}
               {:name "Gwen" :points 0}
-              {:name "Jeff" :points 0}]})
-
+              {:name "Jeff" :points 0}
+              {:name "Yoda" :points 0}]})
 
 (defmulti read-people om/dispatch)
 
