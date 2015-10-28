511af1b65f55e13ac83038bdb17cddd70bde5126
griffio <griffio@users.noreply.github.com>
2015-10-28 19:21:35 +0000

    1.0.0-alpha12
    1.0.0-alpha12 deps updated

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
                  [datascript "0.13.1"]
-                 [org.omcljs/om "1.0.0-alpha11"]
+                 [org.omcljs/om "1.0.0-alpha12"]
                  [org.clojure/test.check "0.8.2"]
                  [com.cognitect/transit-cljs "0.8.225"]
                  [figwheel-sidecar "0.4.0" :scope "provided"]])
