44352e3ee43e3e1da425e5a1bfa888fe664c9d96
griffio <griffio@users.noreply.github.com>
2016-03-31 17:03:14 +0100

    deps 1.0.0-alpha32

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject om-next-tutorial "0.1.0-SNAPSHOT"
   :description "Om-next program"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.170"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.40"]
                  [datascript "0.13.1"]
-                 [org.omcljs/om "1.0.0-alpha30"]
+                 [org.omcljs/om "1.0.0-alpha32"]
                  [org.clojure/test.check "0.8.2"]
                  [com.cognitect/transit-cljs "0.8.225"]
                  [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "provided"]])
