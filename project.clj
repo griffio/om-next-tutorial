f1475718a3394d5f58b3a9d25262a017d2260b3f
griffio <griffio@users.noreply.github.com>
2016-05-18 09:58:29 +0100

    update deps "1.0.0-alpha35"

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.40"]
                  [datascript "0.13.1"]
-                 [org.omcljs/om "1.0.0-alpha34"]
+                 [org.omcljs/om "1.0.0-alpha35"]
                  [org.clojure/test.check "0.8.2"]
                  [com.cognitect/transit-cljs "0.8.225"]
                  [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "provided"]])
