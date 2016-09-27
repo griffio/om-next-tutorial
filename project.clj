06b787fe296a5219bab20ba28ad8e6961173eb9d
griff і⊙ <griffio@users.noreply.github.com>
2016-09-27 10:15:54 +0100

    1.0.0-alpha46

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.89"]
                  [datascript "0.13.1"]
-                 [org.omcljs/om "1.0.0-alpha45"]
+                 [org.omcljs/om "1.0.0-alpha46"]
                  [org.clojure/test.check "0.8.2"]
                  [com.cognitect/transit-cljs "0.8.239"]
                  [figwheel-sidecar "0.5.7" :scope "provided"]])
