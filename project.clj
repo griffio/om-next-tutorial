692026fe3b9afbfce5489add2e19cbf84c033a39
griff і⊙ <griffio@users.noreply.github.com>
2017-04-21 20:47:48 +0100

    update deps Clojurescript 1.9.521
    Om 1.0.0-alpha48

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject om-next-tutorial "0.1.0-SNAPSHOT"
   :description "Om-next program"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.89"]
+                 [org.clojure/clojurescript "1.9.521"]
                  [datascript "0.13.1"]
-                 [org.omcljs/om "1.0.0-alpha46"]
+                 [org.omcljs/om "1.0.0-alpha48"]
                  [org.clojure/test.check "0.8.2"]
                  [com.cognitect/transit-cljs "0.8.239"]
                  [figwheel-sidecar "0.5.7" :scope "provided"]])
